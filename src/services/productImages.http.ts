d97993760759d71cf9b48d141ce6429b711f81d2
luis gonzález <65631946+luisangeluis@users.noreply.github.com>
2023-09-22 19:01:29 +0000

    Created function to add product image

--- src/services/productImages.http.ts
+++ src/services/productImages.http.ts
@@ -1,5 +1,8 @@
 import { Request, Response } from "express";
 import { UserTokenAttributes } from "../types";
+import { uploadImage } from "../utils/cloudinary";
+import fs from "fs-extra";
+
 import * as productImagesController from "../controllers/productImage.controller";
 import * as productController from "../controllers/product.controller";
 
@@ -49,11 +52,9 @@ export const postImageByProductId = async (
   try {
     const productId = req.params.id;
     const userId = (req.user as UserTokenAttributes)?.id;
-
-    // const files = await req.files?.image;
-    console.log({ files: req.files });
-
     const query = { sellerId: userId };
+    const tempFile: any = req.files?.product_image;
+
     const product = await productController.readProductById(productId, query);
 
     if (!product)
@@ -61,14 +62,16 @@ export const postImageByProductId = async (
         .status(404)
         .json({ message: `Product with id: ${productId} doesn't exists` });
 
-    // console.log(req.files);
+    const uploadedImage = await uploadImage(tempFile.tempFilePath);
 
-    // const newProductImage = { productId, url: "" };
+    const newProductImage = { productId, url: uploadedImage.secure_url };
 
-    // const response =
-    //   await productImagesController.createProductImage(newProductImage);
+    const response =
+      await productImagesController.createProductImage(newProductImage);
 
-    return res.status(201).json({ message: "product image created" });
+    await fs.unlink(tempFile.tempFilePath);
+
+    return res.status(201).json({ response });
   } catch (error: any) {
     return res.status(500).json({ message: error.message });
   }
