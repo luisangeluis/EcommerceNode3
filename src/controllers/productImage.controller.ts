cc4969310ae66495171008d098a165b15a68ef10
luis gonzález <65631946+luisangeluis@users.noreply.github.com>
2023-09-25 19:54:46 +0000

    method to delete a product image builded

--- src/controllers/productImage.controller.ts
+++ src/controllers/productImage.controller.ts
@@ -2,6 +2,8 @@ import { v4 as uuidv4 } from "uuid";
 
 import ProductImage from "../models/ProductImage.model";
 import type { ProductImageCreationAttributes } from "../types";
+import { readProductById } from "./product.controller";
+import Product from "../models/Product.model";
 
 export const readAllImagesByProductId = async (productId: string) => {
   const response = await ProductImage.findAll({ where: { productId } });
@@ -26,4 +28,24 @@ export const createProductImage = async (
   const response = await ProductImage.create({ ...productImage, id: uuidv4() });
 
   return response;
+};
+
+export const deleteProductImage = async (
+  sellerId: string,
+  productId: string,
+  productImageId: string,
+) => {
+  const response = await Product.findOne({
+    where: { id: productId, sellerId },
+    include: {
+      model: ProductImage,
+      where: {
+        id: productImageId,
+      },
+    },
+  });
+
+  if (!response) return 0;
+
+  return await ProductImage.destroy({ where: { id: productImageId } });
 };
