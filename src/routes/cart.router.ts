a30fc6e1595d3458427fca078fef686610289234
luis gonzález <65631946+luisangeluis@users.noreply.github.com>
2024-04-26 23:11:03 +0000

    Modified auth to create an order

--- src/routes/cart.router.ts
+++ src/routes/cart.router.ts
@@ -7,6 +7,6 @@ import * as orderServices from "../services/order.http";
 const router = Router();
 
 router.route("/").get(passport.authenticate("jwt", { session: false }), IsACustomer, cartServices.getCartByUserId);
-router.route("/make-order").post(passport.authenticate("jwt", { session: false }), IsACustomer, orderServices.post);
+router.route("/make-order").post(passport.authenticate("jwt", { session: false }), orderServices.post);
 
 export default router;
